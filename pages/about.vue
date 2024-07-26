ee8d2e9df8490ce842593ead5569862ce362a4ca
tcastanie <thibaut.castanie@gmail.com>
2024-07-26 18:29:54 +0200

    💬 Update text

--- pages/about.vue
+++ pages/about.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="px-8 mt-8 mb-16 grid gap-8">
-    <p>ChatGPT is the new Lorem Ipsum</p>
     <ul class="list-disc list-inside">
       <p>Some images are from Unsplash:</p>
       <li>
@@ -18,5 +17,15 @@
         >@mnelson</NuxtLink>
       </li>
     </ul>
+    <p>
+      Made by <NuxtLink
+        to="https://tcastanie.dev"
+        target="_blank"
+        class="underline underline-offset-2"
+      >
+        tcastanie.dev
+      </NuxtLink>
+    </p>
+    <quote>ChatGPT is the new Lorem Ipsum</quote>
   </div>
 </template>
